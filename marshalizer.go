10846750d06ca0ad63935a2899f0a636874042bd
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-12-31 10:42:29 +0200

    marshalizer refactiring

--- marshalizer.go
+++ marshalizer.go
@@ -9,10 +9,116 @@ import (
 	"strings"
 )
 
+type Serializer func(val reflect.Value, includePrivateFields bool) any
+
+type Marshalizer struct {
+	context map[uintptr]string
+}
+
+func SerializePointer(val reflect.Value, includePrivateFields bool) any {
+	// Handle pointer types
+	if val.IsNil() {
+		return nil
+	}
+
+	// Create a structure for pointers
+	ptrData := map[string]any{
+		"address": fmt.Sprintf("%p", val.Interface()), // Pointer address
+		// "address": id(val.Interface()),                // Pointer address
+		"type": fmt.Sprintf("%T", val.Interface()), // Pointer type
+	}
+
+	// Serialize the dereferenced value first
+	value := serializeUnsupported(val.Elem().Interface(), includePrivateFields)
+
+	// If the dereferenced value is a map (structure-like), add pointer data
+	if reflect.ValueOf(value).Kind() == reflect.Map {
+		// Add pointer metadata to the map
+		value.(map[string]any)["*"] = ptrData
+		return value
+	}
+	// If it's not a map, just return the value as is
+	return value
+}
+
+func SerializeStruct(val reflect.Value, includePrivateFields bool) any {
+	// Handle structs
+	m := make(map[string]any)
+	typ := val.Type()
+	// typ := reflect.TypeOf(object)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+		if !field.CanInterface() {
+			// optional
+			if includePrivateFields {
+				m[fieldType.Name] = "[private]"
+			}
+			continue // Skip unexported fields
+		}
+		m[fieldType.Name] = serializeUnsupported(field.Interface(), includePrivateFields)
+	}
+	return m
+}
+
+func SerializeSlice(val reflect.Value, includePrivateFields bool) any {
+	// Handle slices
+	result := make([]any, val.Len())
+	for i := 0; i < val.Len(); i++ {
+		result[i] = serializeUnsupported(val.Index(i).Interface(), includePrivateFields)
+	}
+	return result
+}
+
+func SerializeMap(val reflect.Value, includePrivateFields bool) any {
+	// Handle maps
+	result := make(map[string]any)
+	for _, key := range val.MapKeys() {
+		result[fmt.Sprintf("%v", key.Interface())] = serializeUnsupported(val.MapIndex(key).Interface(), includePrivateFields)
+	}
+	return result
+}
+
+func SerializeFuncSignature(val reflect.Value, includePrivateFields bool) any {
+	// Automatically generate a function descriptor
+	funcType := val.Type()
+
+	// Get parameter types of the function (if any)
+	numParams := funcType.NumIn()
+	paramTypes := []string{}
+	for i := 0; i < numParams; i++ {
+		if typestring := funcType.In(i).String(); len(typestring) > 0 {
+			paramTypes = append(paramTypes, removeSpaces(typestring))
+		}
+	}
+
+	funcName := funcType.Name()
+	if len(funcName) > 0 {
+		funcName += " "
+	}
+
+	numReturns := funcType.NumOut()
+	returnTypes := []string{}
+	for i := 0; i < numReturns; i++ {
+		if typestring := funcType.Out(i).String(); len(typestring) > 0 {
+			paramTypes = append(paramTypes, removeSpaces(typestring))
+		}
+	}
+	
+	fmt.Println(joinTypes(paramTypes), joinTypes(returnTypes))
+	if len(returnTypes) == 0 {
+		return fmt.Sprintf("%sfunc(%s)", funcName, joinTypes(paramTypes))
+	} else if len(returnTypes) == 1 {
+		return fmt.Sprintf("%sfunc(%s) %s", funcName, joinTypes(paramTypes), joinTypes(returnTypes))
+	} else {
+		return fmt.Sprintf("%sfunc(%s) (%s)", funcName, joinTypes(paramTypes), joinTypes(returnTypes))
+	}
+}
+
 // CustomMarshal replaces unsupported types with string descriptors and controls HTML escaping.
-func CustomMarshal(v interface{}, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
+func CustomMarshal(object any, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
 	// Marshal data with custom serialization
-	serializedData := serializeUnsupported(v, includePrivateFields)
+	serializedData := serializeUnsupported(object, includePrivateFields)
 
 	// Marshal data with indentation and optional HTML escaping
 	var result []byte
@@ -35,137 +141,26 @@ func CustomMarshal(v interface{}, escapeHTML bool, includePrivateFields bool) ([
 }
 
 // serializeUnsupported replaces unsupported types like functions with string descriptors.
-func serializeUnsupported(v interface{}, includePrivateFields bool) interface{} {
-	if v == nil {
+func serializeUnsupported(object any, includePrivateFields bool) any {
+	if object == nil {
 		return nil
 	}
 
-	val := reflect.ValueOf(v)
+	val := reflect.ValueOf(object)
 	switch val.Kind() {
 	case reflect.Pointer:
-		// Handle pointer types
-		if val.IsNil() {
-			return nil
-		}
-
-		// Create a structure for pointers
-		ptrData := map[string]interface{}{
-			"address": fmt.Sprintf("%p", val.Interface()), // Pointer address
-			// "address": id(val.Interface()),                // Pointer address
-			"type": fmt.Sprintf("%T", val.Interface()), // Pointer type
-		}
-
-		// Serialize the dereferenced value first
-		value := serializeUnsupported(val.Elem().Interface(), includePrivateFields)
-
-		// If the dereferenced value is a map (structure-like), add pointer data
-		if reflect.ValueOf(value).Kind() == reflect.Map {
-			// Add pointer metadata to the map
-			value.(map[string]interface{})["*"] = ptrData
-			return value
-		}
-		// If it's not a map, just return the value as is
-		return value
-
-		// // Handle pointer types
-		// if val.IsNil() {
-		// 	return nil
-		// }
-
-		// // // Create a structure for pointers
-		// // ptrData := map[string]interface{}{
-		// // 	"address": fmt.Sprintf("%p", val.Interface()), // Pointer address
-		// // 	"type":    fmt.Sprintf("%T", val.Interface()), // Pointer type
-		// // }
-		// // return map[string]interface{}{
-		// // 	"*":     ptrData,                                                            // Store pointer data under "*"
-		// // 	"value": serializeUnsupported(val.Elem().Interface(), includePrivateFields), // Dereferenced value
-		// // }
-
+		return SerializePointer(val, includePrivateFields)
 	case reflect.Struct:
-		// Handle structs
-		m := make(map[string]interface{})
-		typ := reflect.TypeOf(v)
-		for i := 0; i < val.NumField(); i++ {
-			field := val.Field(i)
-			fieldType := typ.Field(i)
-			if !field.CanInterface() {
-				// optional
-				if includePrivateFields {
-					m[fieldType.Name] = "[private]"
-				}
-				continue // Skip unexported fields
-			}
-			m[fieldType.Name] = serializeUnsupported(field.Interface(), includePrivateFields)
-		}
-		return m
-
+		return SerializeStruct(val, includePrivateFields)
 	case reflect.Slice:
-		// Handle slices
-		result := make([]interface{}, val.Len())
-		for i := 0; i < val.Len(); i++ {
-			result[i] = serializeUnsupported(val.Index(i).Interface(), includePrivateFields)
-		}
-		return result
-
+		return SerializeSlice(val, includePrivateFields)
 	case reflect.Map:
-		// Handle maps
-		result := make(map[string]interface{})
-		for _, key := range val.MapKeys() {
-			result[fmt.Sprintf("%v", key.Interface())] = serializeUnsupported(val.MapIndex(key).Interface(), includePrivateFields)
-		}
-		return result
-
+		return SerializeMap(val, includePrivateFields)
+	case reflect.Func:
+		return SerializeFuncSignature(val, includePrivateFields)
 	default:
-		if val.Kind() == reflect.Func {
-			// Automatically generate a function descriptor
-			funcType := val.Type()
-
-			// Get parameter types of the function (if any)
-			numParams := funcType.NumIn()
-			// paramTypes := make([]string, numParams)
-			paramTypes := []string{}
-			for i := 0; i < numParams; i++ {
-				// paramTypes[i] = funcType.In(i).String()
-				if typestring := funcType.In(i).String(); len(typestring) > 0 {
-					// paramTypes[i] = typestring
-					paramTypes = append(paramTypes, removeSpaces(typestring))
-				}
-			}
-
-			// // Get return type if available
-			// var returnType string
-			// if funcType.NumOut() > 0 {
-			// 	returnType = funcType.Out(0).String()
-			// }
-
-			numReturns := funcType.NumOut()
-			// returnTypes := make([]string, numParams)
-			returnTypes := []string{}
-			for i := 0; i < numReturns; i++ {
-				if typestring := funcType.Out(i).String(); len(typestring) > 0 {
-					// returnTypes[i] = typestring
-					paramTypes = append(paramTypes, removeSpaces(typestring))
-				}
-			}
-
-			// // Create a descriptor string that includes the function name, parameter types, and return type if any
-			// if returnType != "" {
-			// 	return fmt.Sprintf("func(%s) %s", joinTypes(paramTypes), returnType)
-			// } else {
-			// 	return fmt.Sprintf("func(%s)", joinTypes(paramTypes))
-			// }
-
-			if len(returnTypes) == 0 {
-				return fmt.Sprintf("func(%s)", joinTypes(paramTypes))
-			} else if len(returnTypes) == 1 {
-				return fmt.Sprintf("func(%s) %s", joinTypes(paramTypes), joinTypes(returnTypes))
-			} else {
-				return fmt.Sprintf("func(%s) (%s)", joinTypes(paramTypes), joinTypes(returnTypes))
-			}
-		}
 		// Return the value directly for supported types
-		return v
+		return object
 	}
 }
 
@@ -193,7 +188,7 @@ func removeSpaces(typeStr string) string {
 // )
 
 // // CustomMarshal replaces unsupported types with string descriptors and controls HTML escaping.
-// func CustomMarshal(v interface{}, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
+// func CustomMarshal(object any, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
 // 	// Marshal data with custom serialization
 // 	serializedData := serializeUnsupported(v, includePrivateFields)
 
@@ -218,7 +213,7 @@ func removeSpaces(typeStr string) string {
 // }
 
 // // serializeUnsupported replaces unsupported types like functions with string descriptors.
-// func serializeUnsupported(v interface{}, includePrivateFields bool) interface{} {
+// func serializeUnsupported(object any, includePrivateFields bool) any {
 // 	if v == nil {
 // 		return nil
 // 	}
@@ -234,7 +229,7 @@ func removeSpaces(typeStr string) string {
 // 		result := serializeUnsupported(val.Elem().Interface(), includePrivateFields)
 // 		// If it's a map, add "*" prefix to the struct key only
 // 		if reflect.TypeOf(result).Kind() == reflect.Map {
-// 			m := result.(map[string]interface{})
+// 			m := result.(map[string]any)
 // 			for key := range m {
 // 				// Only add * to the top-level key for the struct
 // 				if key != "*private" { // To avoid adding * to private fields
@@ -246,7 +241,7 @@ func removeSpaces(typeStr string) string {
 // 		return result
 // 	case reflect.Struct:
 // 		// Handle structs
-// 		m := make(map[string]interface{})
+// 		m := make(map[string]any)
 // 		typ := reflect.TypeOf(v)
 // 		for i := 0; i < val.NumField(); i++ {
 // 			field := val.Field(i)
@@ -263,14 +258,14 @@ func removeSpaces(typeStr string) string {
 // 		return m
 // 	case reflect.Slice:
 // 		// Handle slices
-// 		result := make([]interface{}, val.Len())
+// 		result := make([]any, val.Len())
 // 		for i := 0; i < val.Len(); i++ {
 // 			result[i] = serializeUnsupported(val.Index(i).Interface(), includePrivateFields)
 // 		}
 // 		return result
 // 	case reflect.Map:
 // 		// Handle maps
-// 		result := make(map[string]interface{})
+// 		result := make(map[string]any)
 // 		for _, key := range val.MapKeys() {
 // 			result[fmt.Sprintf("%v", key.Interface())] = serializeUnsupported(val.MapIndex(key).Interface(), includePrivateFields)
 // 		}
@@ -323,7 +318,7 @@ func removeSpaces(typeStr string) string {
 // )
 
 // // CustomMarshal replaces unsupported types with string descriptors and controls HTML escaping.
-// func CustomMarshal(v interface{}, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
+// func CustomMarshal(object any, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
 // 	// Marshal data with custom serialization
 // 	serializedData := serializeUnsupported(v, includePrivateFields)
 
@@ -348,7 +343,7 @@ func removeSpaces(typeStr string) string {
 // }
 
 // // serializeUnsupported replaces unsupported types like functions with string descriptors.
-// func serializeUnsupported(v interface{}, includePrivateFields bool) interface{} {
+// func serializeUnsupported(object any, includePrivateFields bool) any {
 // 	if v == nil {
 // 		return nil
 // 	}
@@ -364,7 +359,7 @@ func removeSpaces(typeStr string) string {
 // 		result := serializeUnsupported(val.Elem().Interface(), includePrivateFields)
 // 		// If it's a pointer, prepend "*" to the key
 // 		if reflect.TypeOf(result).Kind() == reflect.Map {
-// 			m := result.(map[string]interface{})
+// 			m := result.(map[string]any)
 // 			for key := range m {
 // 				m["*"+key] = m[key] // Add '*' prefix to each key
 // 				delete(m, key)      // Remove the old key
@@ -373,7 +368,7 @@ func removeSpaces(typeStr string) string {
 // 		return result
 // 	case reflect.Struct:
 // 		// Handle structs
-// 		m := make(map[string]interface{})
+// 		m := make(map[string]any)
 // 		typ := reflect.TypeOf(v)
 // 		for i := 0; i < val.NumField(); i++ {
 // 			field := val.Field(i)
@@ -390,14 +385,14 @@ func removeSpaces(typeStr string) string {
 // 		return m
 // 	case reflect.Slice:
 // 		// Handle slices
-// 		result := make([]interface{}, val.Len())
+// 		result := make([]any, val.Len())
 // 		for i := 0; i < val.Len(); i++ {
 // 			result[i] = serializeUnsupported(val.Index(i).Interface(), includePrivateFields)
 // 		}
 // 		return result
 // 	case reflect.Map:
 // 		// Handle maps
-// 		result := make(map[string]interface{})
+// 		result := make(map[string]any)
 // 		for _, key := range val.MapKeys() {
 // 			result[fmt.Sprintf("%v", key.Interface())] = serializeUnsupported(val.MapIndex(key).Interface(), includePrivateFields)
 // 		}
@@ -450,7 +445,7 @@ func removeSpaces(typeStr string) string {
 // )
 
 // // CustomMarshal replaces unsupported types with string descriptors and controls HTML escaping.
-// func CustomMarshal(v interface{}, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
+// func CustomMarshal(object any, escapeHTML bool, includePrivateFields bool) ([]byte, error) {
 // 	// Marshal data with custom serialization
 // 	serializedData := serializeUnsupported(v, includePrivateFields)
 
@@ -475,7 +470,7 @@ func removeSpaces(typeStr string) string {
 // }
 
 // // serializeUnsupported replaces unsupported types like functions with string descriptors.
-// func serializeUnsupported(v interface{}, includePrivateFields bool) interface{} {
+// func serializeUnsupported(object any, includePrivateFields bool) any {
 // 	if v == nil {
 // 		return nil
 // 	}
@@ -490,7 +485,7 @@ func removeSpaces(typeStr string) string {
 // 		return serializeUnsupported(val.Elem().Interface(), includePrivateFields)
 // 	case reflect.Struct:
 // 		// Handle structs
-// 		m := make(map[string]interface{})
+// 		m := make(map[string]any)
 // 		typ := reflect.TypeOf(v)
 // 		for i := 0; i < val.NumField(); i++ {
 // 			field := val.Field(i)
@@ -507,14 +502,14 @@ func removeSpaces(typeStr string) string {
 // 		return m
 // 	case reflect.Slice:
 // 		// Handle slices
-// 		result := make([]interface{}, val.Len())
+// 		result := make([]any, val.Len())
 // 		for i := 0; i < val.Len(); i++ {
 // 			result[i] = serializeUnsupported(val.Index(i).Interface(), includePrivateFields)
 // 		}
 // 		return result
 // 	case reflect.Map:
 // 		// Handle maps
-// 		result := make(map[string]interface{})
+// 		result := make(map[string]any)
 // 		for _, key := range val.MapKeys() {
 // 			result[fmt.Sprintf("%v", key.Interface())] = serializeUnsupported(val.MapIndex(key).Interface(), includePrivateFields)
 // 		}
